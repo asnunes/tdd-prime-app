280b60ef50af531e850ef76214c0d441809904ff
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-06-05 14:46:33 -0300

    Added test to given number equal 2

--- tests/services/primeFactor.test.js
+++ tests/services/primeFactor.test.js
@@ -6,9 +6,17 @@ describe("primeFactor", () => {
     const n = 1;
 
     //ACT
-    const result = primeFactorOf(1);
+    const result = primeFactorOf(n);
 
     //ASSERT
     expect(result).toEqual([]);
+  });
+
+  it("should return list with 2 when given number is 2", () => {
+    const n = 2;
+
+    const result = primeFactorOf(n);
+
+    expect(result).toEqual([2]);
   });
 });
