39f70d1ef843362739e8d0a3b8056eb0df31895e
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2020-06-05 15:39:35 -0300

    Implemented generic prime factor algorithm

--- services/primeFactor.js
+++ services/primeFactor.js
@@ -1,21 +1,15 @@
 module.exports = function primeFactorsOf(n) {
   const factors = [];
   let remainder = n;
+  let divisor = 2;
 
-  if (n > 1) {
-    while (remainder % 2 === 0) {
-      factors.push(2);
-      remainder = remainder / 2;
+  while (remainder > 1) {
+    while (remainder % divisor === 0) {
+      factors.push(divisor);
+      remainder = remainder / divisor;
     }
 
-    while (remainder % 3 === 0) {
-      factors.push(3);
-      remainder = remainder / 3;
-    }
-  }
-
-  if (remainder > 1) {
-    factors.push(remainder);
+    divisor++;
   }
 
   return factors;
